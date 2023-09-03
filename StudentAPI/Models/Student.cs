621723bb0c11fecefd16bd466f589ac84c09735e
bagasamrii <amri.bagas7@gmail.com>
2023-09-03 12:20:47 +0700

    firstcommit

---
+++ StudentAPI/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StudentAPI.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentID { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(250)")]
+        public string Name { get; set; } = "";
+
+        public string ContactNumber { get; set; }
+
+        public string Age { get; set; }
+    }
+}
